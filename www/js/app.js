2e1c60275397f864a486f88fbf44db2fe92c9fbc
Felipe Mammoli <fmammoli@gmail.com>
2015-01-22 12:09:11 -0200

    google plus web login

---
+++ www/js/app.js
@@ -0,0 +1,48 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic','googleplus'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(['GooglePlusProvider', function(GooglePlusProvider) {
+     GooglePlusProvider.init({
+        clientId: '525447299557-32gmltal4ahki7tqnntdq4b1berbevgk.apps.googleusercontent.com',
+        apiKey: 'c6M5Z4pP13KONpwl4vgVvQEa'
+     });
+}])
+
+.controller('LoginCtrl', function ($scope, GooglePlus) {
+  $scope.facebookLogin = function () {
+    alert('Facebook Login');
+  }
+
+  $scope.googlePlusLogin = function () {
+    GooglePlus.login().then(function (res) {
+      GooglePlus.getUser().then(function (user) {
+        $scope.user = user;
+      });
+
+    }, function (err) {
+      console.log(err)
+    })
+  }
+
+  $scope.logout = function () {
+    gapi.auth.signOut();
+    $scope.user = null;
+  }
+})
